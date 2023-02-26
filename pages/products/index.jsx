26a35393a626fcc81e6c8f4e6bde3b951e16c2ce
KaungKhantSiThu <64033794+KaungKhantSiThu@users.noreply.github.com>
2023-02-26 21:46:30 +0700

    still fixing .env

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -49,7 +49,7 @@ export default function Home({ products }) {
   )
 }
 export async function getServerSideProps() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/`)
   const products = await res.json()
   return { props: { products } }
 }
